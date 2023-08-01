f54544f33a8149a063f624f4b4335b4464e11e55
aadarsh vishwakarma <aadarshvishwakarma007@gmail.com>
2023-08-01 23:20:22 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,9 @@
+
+const heading = React.createElement("div", { id: "parent"}, 
+                React.createElement("div", { id: "child"}, 
+                React.createElement("h1", {}, "Hello World from React !!")
+                ));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
